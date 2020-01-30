004d340a662cff1c0115d730a899935fb593da67
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-30 06:16:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,13 @@
 class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-
+    if req.path == "/songs"
+      resp.write "You requested shit"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
 end

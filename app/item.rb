5575d9ce16004c9fd535c5da8dfa818fda30f835
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-30 06:42:03 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -1,25 +1,25 @@
 class Item
   attr_accessor :name, :price
-  @@all = []
+  # @@all = []
 
   def initialize(name,price)
     @name = name
     @price = price
-
-    @@all << self
+    #
+    # @@all << self
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.find_by_name(name_of_item)
-    the_item = nil
-    Item.all.each do |item|
-      if item.name == name_of_item
-        the_item = item
-      end
-    end
-    the_item
-  end
+  # def self.all
+  #   @@all
+  # end
+  #
+  # def self.find_by_name(name_of_item)
+  #   the_item = nil
+  #   Item.all.each do |item|
+  #     if item.name == name_of_item
+  #       the_item = item
+  #     end
+  #   end
+  #   the_item
+  # end
 end
